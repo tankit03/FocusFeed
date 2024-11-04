1389ed1a4e53380676b73484cfd07b858cc2c703
Tanish Hupare <huparetanish@gmail.com>
2024-11-03 17:17:59 -0800

    Superbase set up

---
+++ src/app/notes/page.tsx
@@ -0,0 +1,10 @@
+"use server";
+
+import { createClient } from '@/utils/supabase/server';
+
+export default async function Notes() {
+  const supabase = await createClient();
+  const { data: notes } = await supabase.from("notes").select();
+
+  return <pre>{JSON.stringify(notes, null, 2)}</pre>
+}
